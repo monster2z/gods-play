69582f08a7b8abe78f143997aae91a891a8d3983
lee monster <monster2z.mars@gmail.com>
2026-09-06 22:39:18 +0900

    설산 눈 표현 재작업 + 낙엽 붉은 계열

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788699668|2018983';
+const CACHE_VERSION = '1788701954|2030213';
 /** @type {string} */
 const CACHE_PREFIX = '벌의 신-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
