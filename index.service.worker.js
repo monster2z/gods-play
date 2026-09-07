8c44f688c2c014e07121d70aa8b7c7e688c63cd4
lee monster <monster2z.mars@gmail.com>
2026-09-08 06:47:41 +0900

    ship 2026-09-08 06:47

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788701954|2030213';
+const CACHE_VERSION = '1788817658|2002006';
 /** @type {string} */
 const CACHE_PREFIX = '벌의 신-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
